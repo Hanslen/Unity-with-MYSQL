f9c0b4195fd6565267a179c05ebfea6ce587bbce
Jiahe Chen <psyjc3@exmail.nottingham.ac.uk>
2016-06-06 17:53:37 +0800

    connect with the database successfully
    _mysql->Database Handler(script): to change the database setting

---
+++ Assets/Scripts/DatabaseHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Data;
+using System.Text;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class DatabaseHandler : MonoBehaviour {
+	public string host, database, user, password;
+	public bool pooling = true;
+
+	private string connectionString;
+	private MySqlConnection con = null;
+	private MySqlCommand cmd = null;
+	private MySqlDataReader rdr = null;
+
+	private MD5 _md5Hash;
+
+	void Awake(){
+		DontDestroyOnLoad (this.gameObject);
+		connectionString = "Server=" + host + ";Database=" + database + ";User=" + user + ";Password=" + password + ";Pooling=";
+		if (pooling) {
+			connectionString += "true";
+		} else {
+			connectionString += "false";
+		}
+		try{
+			con = new MySqlConnection(connectionString);
+			con.Open();
+			Debug.Log("Mysql state: "+con.State);
+		}catch(Exception e){
+			Debug.Log (e);
+		}
+	}
+	void onApplicationQuit(){
+		if (con != null) {
+			if (con.State.ToString () != "Closed") {
+				con.Close ();
+				Debug.Log ("Mysql connection closed");
+			}
+			con.Dispose ();
+		}
+	}
+
+	public string GetConnectionState(){
+		return con.State.ToString ();
+	}
+}
---
+++ Assets/Scripts/LoginGUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginGUI : MonoBehaviour {
+	public DatabaseHandler _mysqlHolder;
+
+	void OnGUI(){
+		GUI.Label (new Rect (10, 10, 300, 30), "MYSQL Connection state："+_mysqlHolder.GetConnectionState ());
+	}
+}
